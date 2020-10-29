2fbf1b018976af3730b5cb5f098d80003b545ebb
Daniel Moreira <danieljt84@gmail.com>
2020-10-28 21:21:26 -0300

    Bootstrap instalado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'alurapic';
+  url='https://img.ibxk.com.br/2020/01/30/30021141299110.jpg?w=1120&h=420&mode=crop&scale=both';
+  
 }
