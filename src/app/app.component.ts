6802ae650d6b839ef3e88aa1040bfd56d3a71991
Daniel Moreira <danieljt84@gmail.com>
2020-11-27 16:58:39 -0300

    API adicionada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Photo } from './photos/photo/photo';
+import { PhotoService } from './photos/photo/photo.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'alurapic';
-  url='https://img.ibxk.com.br/2020/01/30/30021141299110.jpg?w=1120&h=420&mode=crop&scale=both';
+export class AppComponent implements OnInit{
+  
+  photos: Photo[] = [];
+
+  constructor(private photoService: PhotoService ){
+  }
+
+  ngOnInit(): void {
+    this.photoService
+    .listFromUser('flavio')
+    .subscribe(photos =>{ 
+      this.photos = photos
+    });
+  }
   
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import {PhotosModules} from './photos/photos.module'
+import { HttpClientModule } from '@angular/common/http';
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    PhotosModules,
+    HttpClientModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
