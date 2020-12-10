9d7e2b7553ac4487166f48b27ba2b60ca9785b50
Daniel Moreira <danieljt84@gmail.com>
2020-12-10 19:46:42 -0300

    Diretiva adicionanada - App 1.0

--- src/app/photos/photo-list/photo-list.module.ts
+++ src/app/photos/photo-list/photo-list.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { CardModule } from 'src/app/shared/components/card/card.module';
+import { DarkenOnHoverModule } from 'src/app/shared/directives/darken-on-hover/darken.on.hoven.module';
 import { PhotoModule } from '../photo/photo.module';
 import { FilterByDescription } from './filter-by-description.pipe';
 import { LoadButtonComponent } from './load-button/load-button.component';
@@ -11,7 +12,7 @@ import { SearchComponent } from './search/search.component';
 
 @NgModule({
     declarations:[PhotoListComponent,LoadButtonComponent,PhotosComponent,FilterByDescription,SearchComponent],
-    imports:[CommonModule,PhotoModule,CardModule],
+    imports:[CommonModule,PhotoModule,CardModule,DarkenOnHoverModule],
 
 })
 export class PhotoListModule {}
