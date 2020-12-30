7e3f7b069af421b16ca13faa04960c480e94136d
Daniel Moreira <danieljt84@gmail.com>
2020-12-29 22:25:24 -0300

    Component do login

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { SiginComponent } from './signin/signon.component';
+
+@NgModule({
+    declarations: [SiginComponent]
+})
+export class HomeModule {
+
+}
