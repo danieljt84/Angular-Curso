abf86612409c44bf0066010d013a06f8bb1543c2
Daniel Moreira <danieljt84@gmail.com>
2020-12-30 12:42:14 -0300

    validacao do formulario

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { SiginComponent } from './signin/signon.component';
-
+import {FormBuilder, FormGroup, ReactiveFormsModule} from '@angular/forms'
+import { CommonModule } from '@angular/common';
 @NgModule({
-    declarations: [SiginComponent]
+    declarations: [SiginComponent],
+    imports: [ReactiveFormsModule,CommonModule]
 })
 export class HomeModule {
 
