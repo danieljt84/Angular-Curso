54534c80fd3e8e5fa0a4c11cdaa9aad12f04c252
Daniel Moreira <danieljt84@gmail.com>
2020-12-30 17:00:36 -0300

    enviando credencias para a api

--- src/app/home/signin/signon.component.ts
+++ src/app/home/signin/signon.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/core/auth.service';
 
 @Component({
     templateUrl: './signin.component.html'
@@ -8,14 +9,27 @@ export class SiginComponent implements OnInit{
 
     loginForm: FormGroup;
 
-    constructor(private formBuilder: FormBuilder){}
+    constructor(private formBuilder: FormBuilder,private auth: AuthService){}
 
 
     ngOnInit(): void {
         this.loginForm = this.formBuilder.group({
-            userName: [' ',Validators.required],
-            password: [' ',Validators.required]
+            userName: ['',Validators.required],
+            password: ['',Validators.required]
         })
     }
+
+    login(){
+        console.log('vai autenticar');
+        const userName = this.loginForm.get('userName').value;
+        const password = this.loginForm.get('password').value;
+
+        this.auth.authenticate(userName,password).subscribe(()=> console.log('tudo certo'), err =>{ 
+            console.log('erro');
+            this.loginForm.reset();
+        });
+    }
+
+
 
 }
