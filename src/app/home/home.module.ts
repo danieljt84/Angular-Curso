b9ea075f5760ff982365d12ce42b5ea6bddb26ab
Daniel Moreira <danieljt84@gmail.com>
2021-01-21 13:11:58 -0300

    refinando o uso de rotas

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -7,6 +7,7 @@ import { RouterModule } from '@angular/router';
 import { SignUpComponent } from './singup/singup.component';
 import { HomeComponent } from './home/home.component';
 import { HomeRoutingModule } from './home.routing.module';
+import { SignUpService } from './singup/signup.service';
 
 @NgModule({
     declarations: [ 
@@ -21,6 +22,9 @@ import { HomeRoutingModule } from './home.routing.module';
         VMessageModule,
         RouterModule,
         HomeRoutingModule
+    ],
+    providers: [
+        SignUpService
     ]
 })
 export class HomeModule{ }
